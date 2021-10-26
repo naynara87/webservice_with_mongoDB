b3aeb2e06b5b48a09bbb0677af20490483f2b75d
naynara87 <naynara87@gmail.com>
2021-10-26 10:13:27 +0900

    이미지 업로드

--- server.js
+++ server.js
@@ -162,13 +162,21 @@ app.use('/', require('./routes/board.js') );
 
 
 let multer = require('multer');
+var dt;
+dt = new Date();
+
+dt = dt.getFullYear() + (dt.getMonth() + 1) + dt.getDate();
+
 var storage = multer.diskStorage({
   destination : function(req, file, cb){
     cb(null, './public/image')
   },
   filename : function(req, file, cb){
-    cb(null, file.originalname )
+
+    cb(null, 'img' + dt + file.originalname )
   }
+  // filefilter : function(req, file, cb){
+  // }
 
 });
 
@@ -182,6 +190,14 @@ app.get('/upload', function(요청, 응답){
 app.post('/upload', upload.single('프로필'), function(요청, 응답){
   응답.send('업로드완료')
 });
+
+app.get('/image/:imageName', function (요청, 응답){
+  응답.sendFile(__dirname + '/public/image/' + 요청.params.imageName )
+});
+
+
+
+
 
 
 app.post('/add', function (요청, 응답) {
